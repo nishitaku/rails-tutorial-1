11844df19ef5e68584af969b21c735220709d520
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-03-16 11:10:44 +0900

    fix show user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
-    @first_micropost = User.first.microposts.first
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
